a171b8c3597afbf6ac8d2e47f776332251d6e933
Davide Murro <e-demu@gft.com>
2024-10-03 12:11:10 +0200

    little graphic fix
    little graphic fix

--- www.worldisawesome.fun/AppService.cs
+++ www.worldisawesome.fun/AppService.cs
@@ -2,7 +2,7 @@
 {
     public static class AppService
     {
-        public static string Version = "2.5.2"; //Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        public static string Version = "2.5.3"; //Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
         public static string DBVersion = "1.0.0";
         public static string Creator = "Davide Murro";
         public static string Name = "World is Awesome .fun";
